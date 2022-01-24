e316eb6993d834d0ba35e8da983fa5ade7836595
or abdillh <hans.abdillh05@gmail.com>
2022-01-24 10:58:32 +0800

    Install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,5 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main class="bg-slate-800 w-full md: 10/12 lg:4/12 xl:3/12 mx-auto h-screen px-5 py-4">
+    <p>Soccer App</p>
+  </main>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
