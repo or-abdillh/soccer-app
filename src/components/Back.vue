bab9971bfe83fba43555af032f152ed682986783
or abdillh <hans.abdillh05@gmail.com>
2022-02-01 11:51:05 +0800

    BottomBar component always show everywhere

--- src/components/Back.vue
+++ src/components/Back.vue
@@ -11,7 +11,7 @@
 
 <template>
 	<section class="fixed top-0 left-0 right-0 p-5">
-		<div class="w-full px-5 md:w-8/12 lg:w-4/12 xl:w-3/12 md: 10/12 lg:4/12 xl:3/12 mx-auto">
+		<div class="w-full md:w-8/12 lg:w-4/12 xl:w-3/12 mx-auto">
 			<p @click="back" class="text-green-400 text-xl font-medium active:scale-90 duration-300 pointer">
 				<i class="fa fa-chevron-left mr-1"></i>
 				Back
--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- Wrapper -->
-  <div v-if="currentPath !== 'Read'" class="fixed bottom-0 left-0 right-0">
+  <div class="fixed bottom-0 left-0 right-0">
     <!-- Bottom bar -->
     <div class="w-full md:w-8/12 lg:w-4/12 xl:w-3/12 mx-auto bg-slate-800 bg-opacity-100 px-5 py-3 flex justify-between">
       <template v-for="(menu, index) in menus" :key="index">
         <div
-          @click="menuActive = menu.name"
+          @click="move(menu)"
           :class="menuActive === menu.name ? 'text-green-400 font-medium tracking-wide' : 'text-gray-500'"
           class="active:scale-75 duration-300 flex flex-col items-center">
           <i class=" text-2xl" :class="menu.icon"></i>
@@ -19,11 +19,17 @@
 <script setup>
   
   import { ref, computed } from 'vue'
-  import { useRoute } from 'vue-router'
+  import { useRouter } from 'vue-router'
   import menus from '@/menu.js'
 
-  const route = useRoute()
-  const currentPath = computed(() => route.name)
+  const router = useRouter()
   const menuActive = ref('Home');
+
+  const move = menu => {
+  	menuActive.value = menu.name
+  	setTimeout(() => {
+  		router.push({ name: menu.path })
+  	}, 500)
+  }
   
 </script>
